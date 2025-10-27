d36302d8a68572e688ffc36341e2664114d19137
Arthur Mendes <thumendess@gmail.com>
2025-10-27 19:52:12 -0300

    Corrigino script de testes bash para usar somente os arquivos com padrão 1000x<n>

--- tp1-normalize/scripts/runExperiments.sh
+++ tp1-normalize/scripts/runExperiments.sh
@@ -3,19 +3,16 @@
 BIN="./normalize"
 
 VERSOES=("baseline" "quake" "sse")
-N=(1000 10000)
-D=(3 10)
+D=(3 10 100)
 
 mkdir -p results
 RESULT="results/metrics.csv"
 echo "version,N,D,wall,user,sys,memoryKB" > $RESULT
 
-for n in "${N[@]}"; do
-  for d in "${D[@]}"; do
-    INPUT="tests/data_${n}x${d}.csv"
+for d in "${D[@]}"; do
+    INPUT="tests/data_1000x${d}.csv"
     for version in "${VERSOES[@]}"; do
-      echo "Rodando: $version N=$n D=$d"
-      $BIN "$INPUT" "$version" >> $RESULT
+        echo "Rodando: $version N=$n D=$d"
+        $BIN "$INPUT" "$version" >> $RESULT
     done
-  done
 done
